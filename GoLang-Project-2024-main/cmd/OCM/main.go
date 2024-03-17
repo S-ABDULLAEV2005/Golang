0a83e94fc5f5162f0fc37a8e2793f6ae244e1ffe
shakhzod <s_abdullaev@kbtu.kz>
2024-03-18 04:00:28 +0500

    Commited

---
+++ GoLang-Project-2024-main/cmd/OCM/main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"OCM/pkg/OCM/model"
+	"database/sql"
+	"flag"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	_ "github.com/lib/pq"
+)
+
+type config struct {
+	port string
+	env  string
+	db   struct {
+		dsn string
+	}
+}
+
+type application struct {
+	config config
+	models model.Models
+}
+
+func (app *application) run() {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/", app.HomeHandler)     // home page
+	r.HandleFunc("/user", app.UserHandler) // user page
+
+	// Course Singleton
+	r.HandleFunc("/courses/{id}", app.getCourseHandler).Methods("GET")       // Get a specific course
+	r.HandleFunc("/courses", app.createCourseHandler).Methods("POST")        // Create a new course
+	r.HandleFunc("/courses/{id}", app.updateCourseHandler).Methods("PUT")    // Update a specific course
+	r.HandleFunc("/courses/{id}", app.deleteCourseHandler).Methods("DELETE") // Delete a specific course
+
+	log.Printf("Starting server on %s\n", app.config.port)
+	err := http.ListenAndServe(app.config.port, r)
+	log.Fatal(err)
+}
+
+func main() {
+	var cfg config
+	flag.StringVar(&cfg.port, "port", ":8081", "API server port")
+	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
+	flag.StringVar(&cfg.db.dsn, "db-dsn", "postgres://postgres:1234Asdf@localhost:5432/go?sslmode=disable", "PostgreSQL DSN")
+
+	flag.Parse()
+
+	db, err := openDB(cfg)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	defer db.Close()
+
+	app := &application{
+		config: cfg,
+		models: model.NewModels(db),
+	}
+
+	app.run()
+
+}
+
+func openDB(cfg config) (*sql.DB, error) {
+	// Use sql.Open() to create an empty connection pool, using the DSN from the config // struct.
+	db, err := sql.Open("postgres", cfg.db.dsn)
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
+}
---
+++ GoLang-Project-2024-main/pkg/OCM/model/courses.go
@@ -0,0 +1,110 @@
+package model
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+	"time"
+)
+
+type Course struct {
+	CourseId       int    `json:"course_id"`
+	Title          string `json:"title"`
+	Description    string `json:"description"`
+	CourseDuration string `json:"courseDuration"`
+}
+
+var courses = []Course{
+	{
+		CourseId:       1,
+		Title:          "Programming Principles I",
+		Description:    "Starter programming courses",
+		CourseDuration: "3 month",
+	}, {
+		CourseId:       2,
+		Title:          "Programming Principles II",
+		Description:    "Advanced programming courses",
+		CourseDuration: "4 month",
+	}, {
+		CourseId:       3,
+		Title:          "Algorithms and Data Structures",
+		Description:    "This course for student who accepted their fate",
+		CourseDuration: "3 month",
+	}, {
+		CourseId:       4,
+		Title:          "Computer Networks",
+		Description:    "This course will provide good topic for understanding world web",
+		CourseDuration: "5 month",
+	},
+}
+
+func GetCourses() []Course {
+	return courses
+}
+
+func (cm *CourseModel) Get(id int) (*Course, error) {
+	// Query the course from the database.
+	query := `
+        SELECT course_id, title, description, course_duration
+        FROM courses
+        WHERE course_id = $1
+    `
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	course := &Course{}
+	err := cm.DB.QueryRowContext(ctx, query, id).Scan(&course.CourseId, &course.Title, &course.Description, &course.CourseDuration)
+	if err != nil { // nil => null
+		if err == sql.ErrNoRows {
+			// The course was not found
+			return nil, errors.New("courses not found")
+		} else {
+			// Some other error happened
+			return nil, err
+		}
+	}
+
+	return course, nil
+}
+
+func (cm *CourseModel) Insert(course *Course) error {
+	// Insert a new course into the database.
+	query := `
+		INSERT INTO courses (title, description, course_duration) 
+		VALUES ($1, $2, $3) 
+		RETURNING course_id
+		`
+	args := []interface{}{course.Title, course.Description, course.CourseDuration}
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	return cm.DB.QueryRowContext(ctx, query, args...).Scan(&course.CourseId)
+}
+
+func (cm *CourseModel) Update(course *Course) error {
+	// Update a specific course in the database.
+	query := `
+        UPDATE courses
+        SET title = $1, description = $2, course_duration = $3
+        WHERE course_id = $4
+        RETURNING course_id
+        `
+	args := []interface{}{course.Title, course.Description, course.CourseDuration, course.CourseId}
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	return cm.DB.QueryRowContext(ctx, query, args...).Scan(&course.CourseId)
+}
+
+func (cm *CourseModel) Delete(id int) error {
+	// Delete a specific course from the database.
+	query := `
+        DELETE FROM courses
+        WHERE course_id = $1
+        `
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	_, err := cm.DB.ExecContext(ctx, query, id)
+	return err
+}
